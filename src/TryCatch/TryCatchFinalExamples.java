f7e32f6e6dd28d9a5055679425b18be754185a25
dedeep <dedeepdeepu70@gmail.com>
2025-02-16 18:28:12 +0530

    init commit

--- src/TryCatch/TryCatchFinalExamples.java
+++ src/TryCatch/TryCatchFinalExamples.java
@@ -2,13 +2,15 @@ package TryCatch;
 
 public class TryCatchFinalExamples {
     public static void main(String[] args) {
-        int i = 0;
+        int i = 2;
         int j = 0;
 
         int nums[] = new int[5];
+        String str = null;
 
         try{
             j = 20/i;
+            System.out.println(str.length());
             System.out.println(nums[1]);
             System.out.println(nums[5]);
 
@@ -16,6 +18,8 @@ public class TryCatchFinalExamples {
             System.out.println("Cannot divide by Zero"+ e);
         }catch(ArrayIndexOutOfBoundsException e) {
             System.out.println("Stay in your limit."+ e);
+        }catch(Exception e) {
+            System.out.println("Something went wrong"+ e);
         }
         System.out.println(j);
 
